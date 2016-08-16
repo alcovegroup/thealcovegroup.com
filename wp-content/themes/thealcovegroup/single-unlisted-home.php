12c3dad6c5e7703c1506353a4a0de986fabb7545
Alex Hudson <ahudson@alexhudsondesign.com>
2016-08-15 18:26:40 -0700

    attempting to add custom post type single to reference page template

---
+++ wp-content/themes/thealcovegroup/single-unlisted-home.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The Template for displaying single unlisted home
+ *
+ * @package WordPress
+ * @subpackage thealcovegroup
+ */
+?>
+
+
+<?php get_template_part( 'home-details' ); ?>
