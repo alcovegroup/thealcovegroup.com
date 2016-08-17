bfc780d177f5a2bfce4ac65c1dda03bb3bc8f33a
Alex Hudson <ahudson@alexhudsondesign.com>
2016-08-17 08:50:33 -0700

    map init center to address

--- wp-content/themes/thealcovegroup/single-featured_home.php
+++ wp-content/themes/thealcovegroup/single-featured_home.php
@@ -268,16 +268,16 @@
 
 	function initMap() {
 		var homeAddress = "<?php echo the_field( 'address_1' ); ?> <?php echo the_field( 'address_2' ); ?>";
-		alert(homeAddress);
+		// alert(homeAddress);
 		var myLatLng = {lat: 33.5650816, lng: -111.91640030000002};
 		// Create a map object and specify the DOM element for display.
 		var map = new google.maps.Map(document.getElementById('home-map'), {
-			center: myLatLng,
+			center: homeAddress,
 			scrollwheel: false,
 			zoom: 10
 		});
 		var marker = new google.maps.Marker({
-			position: myLatLng,
+			position: homeAddress,
 			map: map,
 			title: 'Home Location'
 		});
