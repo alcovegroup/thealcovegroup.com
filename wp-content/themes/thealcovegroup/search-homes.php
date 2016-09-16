538bb2d4a8bacb07a222b7342f2602a2ec8602fd
Alex Hudson <ahudson@alexhudsondesign.com>
2016-09-15 18:15:59 -0700

    debugged- search-homes page fixed

--- wp-content/themes/thealcovegroup/search-homes.php
+++ wp-content/themes/thealcovegroup/search-homes.php
@@ -25,7 +25,6 @@
           <div class="row form-page-header form-and-blurb">
             <div class="small-12 small-centered medium-11 large-9 columns">
               <h3><?php the_title(); ?></h3>
-              <span>xxx</span>
               <?php if (have_posts()) : while (have_posts()) : the_post();?>
               <?php the_content(); ?>
               <?php endwhile; endif; wp_reset_query(); ?>
