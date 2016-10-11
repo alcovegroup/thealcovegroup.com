78fb0f4ea813c61130e784772629c8c44f7862d2
Alex Hudson <ahudson@alexhudsondesign.com>
2016-10-11 08:21:49 -0700

    debugging logic for show image background

--- wp-content/themes/thealcovegroup/index.php
+++ wp-content/themes/thealcovegroup/index.php
@@ -8,7 +8,7 @@
       <!-- Hero -->
       
       <div id="hero"
-      <?php if (has_post_thumbnail( $post->ID ) && !post_custom('use_video_background') ): ?>
+      <?php if (has_post_thumbnail( $post->ID ) ): ?>
       <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
        style="background-image: url('<?php echo $image[0]; ?>');"
       <?php endif; ?>
@@ -164,6 +164,7 @@
             if ($(window).width() > responsiveWidth) {
               <?php if ( post_custom('use_video_background') ): ?>
               var videoDiv =  '<div class="video-bg"><video id="videoPreload" width="100%" height="100%" preload autoplay loop muted><source src="<?php echo the_field( "mp4_video_file" ); ?>" type="video/mp4"><source src="<?php echo the_field( "webm_video_file" ); ?>" type="video/webm"><source src="<?php echo the_field( "ogg_video_file" ); ?>" type="video/ogg">Your browser does not support the video tag.</video></div>';
+              $('#hero').css("background-image", "none");
               $('#hero').prepend( videoDiv );
               $('video').get(0).play();
               <?php endif; ?>
