e14da9d976f5182aa078f820bbe98c1690b5649c
Alex Hudson <ahudson@alexhudsondesign.com>
2016-12-09 14:42:07 -0800

    changing capability again

--- wp-content/themes/thealcovegroup/functions.php
+++ wp-content/themes/thealcovegroup/functions.php
@@ -61,7 +61,7 @@ add_theme_support( 'post-thumbnails' );
 add_action('admin_menu', 'add_gcf_interface');
 
 function add_gcf_interface() {
-	add_options_page('Global Fields', 'Global Fields', '7', 'functions', 'editglobalcustomfields');
+	add_options_page('Global Fields', 'Global Fields', 'edit_pages', 'functions', 'editglobalcustomfields');
 }
 
 function editglobalcustomfields() {
