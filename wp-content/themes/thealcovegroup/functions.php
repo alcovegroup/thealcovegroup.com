416de09d5e6383a7e15d664e36af9194321a8a61
Alex Hudson <ahudson@alexhudsondesign.com>
2017-03-01 13:24:24 -0800

    trying to add selected logic

--- wp-content/themes/thealcovegroup/functions.php
+++ wp-content/themes/thealcovegroup/functions.php
@@ -103,14 +103,12 @@ function editglobalcustomfields() {
 	<?php $loop = new WP_Query( array( 'post_type' => 'bios') ); ?>
     <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-		<option value="<?php the_title(); ?>"><?php the_title(); ?></option>
+    	<?php $selected = ( the_title(); == get_option('global_listing_agent'); ) ? ' selected="selected"' : ''; ?>
+		<option value="<?php the_title(); ?>" <?php echo $selected ?> ><?php the_title(); ?></option>
 
 	<?php endwhile; wp_reset_query(); ?>
 	<!-- Value should pass Name, Featured Image, and Phone contact to the single listing page where contact info is injected -->
 	</select>
-	<script>
-		alert($('select[name="global_listing_agent"]'));
-	</script>
 	</p>
 
 	<p><input type="submit" name="Submit" value="Update Options" /></p>
