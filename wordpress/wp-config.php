4e2d5148ca6dfdd88aea62b4a597eeaaf08fb088
Shane Gibson <shane@alcove-staging.localdomain>
2019-01-30 23:46:44 +0000

    Initial staging commit
    * Updating wp-config.php for staging environment
    * Updating hardcoded URLs
    * Removing uploads from tracked file cache

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'thealcovegroup');
+define('DB_NAME', 'alcove_wp_database');
 
 /** MySQL database username */
-define('DB_USER', 'ahudson');
+define('DB_USER', 'alcovewordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'alcovemicrosite');
+define('DB_PASSWORD', '68eVoqjiPSBg');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -49,14 +49,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '~[z@i{I.I(-4soe:M||dd-cWa&c5w+&LouyV@`^l`JuKk`Xef.rdJ&T4A@:_UCh^');
+define('SECURE_AUTH_KEY',  'A0|OS{Mwl:(0FWD2#0H1)>6Cs$%SAkG})qhbliZQFc6boKOoY:!>nJz)vC&yU#^R');
+define('LOGGED_IN_KEY',    '^jjjxE_u)UnXB`eF56NE,aKSH$bEV3M C*c@_+KFP3M?z_X`tWJ90o)g2/R57 jE');
+define('NONCE_KEY',        'f!3PVr1e0NEN2rD7^3h}XKp`ou59kVQ{`E?vY?i!2?l P3G>Mw#-nec)zA:H*3mG');
+define('AUTH_SALT',        'KN52sG|hs;hq5TXAhKrsX?6oYm=n-_P7ruF4 [Mx)9~%:mt@?xmNdKl2p] H$-gS');
+define('SECURE_AUTH_SALT', '-]+eZI/F<$ue#ywi-&E+4ZfMhl%3$MUm+L+F;^P<Pgf~m*2Q3Qb+4n^SbP^!|Q4d');
+define('LOGGED_IN_SALT',   'B&6(@tM!HE9/*d-dh#p/cAFTk~`b?{#p&)iKdRj(6bgOJT8-A7[tj3qxh$_?-bue');
+define('NONCE_SALT',       'zB?VIAHsmE@`^i?[{j6CVY*}Npu4V-7$+cVfti|uCC9?ARVf9,VZV~xmKjvxh|Y-');
 
 /**#@-*/
 
@@ -80,14 +80,16 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
 
-// define('MULTISITE', true);
-// define('SUBDOMAIN_INSTALL', false);
-// define('DOMAIN_CURRENT_SITE', 'thealcovegroup.com');
-// define('PATH_CURRENT_SITE', '/');
-// define('SITE_ID_CURRENT_SITE', 1);
-// define('BLOG_ID_CURRENT_SITE', 1);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'staging.thealcovegroup.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 /* That's all, stop editing! Happy blogging. */
 
--- wordpress/wp-content/themes/thealcovegroup/footer.php
+++ wordpress/wp-content/themes/thealcovegroup/footer.php
@@ -4,7 +4,7 @@
       </div> <!-- End of page hide wrapper -->
     <?php endif; ?>
 
-    <?php if ( get_site_url() === "http://thealcovegroup.com" || get_site_url() === "http://thealcovegroup.com/paradise-valley" ) { ?>
+    <?php if ( get_site_url() === "http://staging.thealcovegroup.com" || get_site_url() === "http://staging.thealcovegroup.com/paradise-valley" ) { ?>
     	<script type="text/javascript">
 		var _ss = _ss || [];
 		_ss.push(['_setDomain', 'https://koi-3QEHIZGEXU.marketingautomation.services/net']);
